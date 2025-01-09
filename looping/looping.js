4cedffa900af9a228d744324757fb9175ceaff72
Thanuja2024 <thanujas9393@gmail.com>
2025-01-09 10:22:55 +0530

    msg

--- looping/looping.js
+++ looping/looping.js
@@ -8,3 +8,16 @@
 // }
 
 
+function test1(a,b){
+    console.log("hello");
+    return a+b;
+    console.log("hi");
+
+}
+console.log(test1(5,5));
+
+let x = (a,b) => {return a*b};
+console.log(x(5,5));
+
+let y = (a,b) => a+b;
+console.log(y(100,200));
